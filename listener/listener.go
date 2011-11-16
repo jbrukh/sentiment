7c7aba735ac3900aea77e43e767909a6bd22ea0c
Jake Brukhman <jbrukh@gmail.com>
2011-11-16 02:11:23 -0500

    First working twitter integration.

--- listener/listener.go
+++ listener/listener.go
@@ -2,31 +2,39 @@ package main
 
 import "fmt"
 import "os"
+import "twitterstream"
+import "flag"
+import "strings"
 
 var username string
 var password string
-
+var track *string
 func init() {
-        if len(os.Args) != 3 {
-                    fmt.Print("Usage: <username> <password>\n")
-                            os.Exit(1)
-                                }
-                                    username = os.Args[1]
-                                        password = os.Args[2]
-                                            fmt.Printf("username: %v password: %v\n", username, password)
+    track = flag.String("track", "", "comma-separated list of tracking terms")
+    flag.Parse()
+
+    args := flag.Args()
+    if len(args) != 2 {
+       println("Usage: [flags...] <username> <password>") 
+       os.Exit(1)
+    }
+	username = args[0]
+	password = args[1]
+
 }
 
 func main() {
 
-        stream := make(chan *twitterstream.Tweet)
-            client := twitterstream.NewClient("username", "password")
+	stream := make(chan *twitterstream.Tweet)
+	client := twitterstream.NewClient(username, password)
 
-                err := client.Track([]string{"#OWS"}, stream)
-                    if err != nil {
-                                println(err.String())
-                                    }
-                                        for {
-                                                    tw := <-stream
-                                                            println(tw.User.Screen_name, ": ", tw.Text)
-                                                                }
+    fmt.Printf("track = %v", *track)
+	err := client.Track(strings.Split(*track, ","), stream)
+	if err != nil {
+		println(err.String())
+	}
+	for {
+		tw := <-stream
+		println(tw.User.Screen_name, ": ", tw)
+	}
 }
